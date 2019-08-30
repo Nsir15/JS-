68bd82d9f91e4d5398a2ee6b0204b8e0c2924195
南希 <447108518@qq.com>
2019-08-30 14:21:24 +0800

    之前计算方式有问题，每次不是递增。而是直接操作左右总距离

--- drag/drag事件监听.js
+++ drag/drag事件监听.js
@@ -1,26 +1,39 @@
 
     let box = document.querySelector('.box');
-    let minX = 0,
-        minY = 0,
+    let minL = 0,
+        minT = 0,
         boxW = box.scrollWidth,
         boxH = box.scrollHeight,
-        maxX = document.documentElement.clientWidth - boxW,
-        maxY = document.documentElement.clientHeight - boxH,
-        originLeft = utils.CSS(box, 'left'),
-        originTop = utils.CSS(box, 'top');
+        maxL = document.documentElement.clientWidth - boxW,
+        maxT = document.documentElement.clientHeight - boxH;
+        // originLeft = utils.CSS(box, 'left'),
+        // originTop = utils.CSS(box, 'top');
 
     const moveStart = function(){
 
-        document.addEventListener('mousemove',moving);
+        
+        // 进来获取初始的left 和 top 
+
+        // 此时的this  是 box
+        // this.startL = utils.CSS(box,'left');
+        // this.startT = utils.CSS(box,'top');
+        // 使用全局变量形式
+        originLeft = utils.CSS(box, 'left');
+        originTop = utils.CSS(box,'top');
+        
+        //因为绑定的是document，所以moving的this 是document,要想继续使用startL,要么设置为全局变量，要么设置为this属性，这里绑定this,使this 仍然是box
+        // 在bind原理那块得出结论：同一个方法绑定同一个this，返回的是一个匿名函数，所以绑定多次返回的结果是不一样的。
+        document.addEventListener('mousemove',moving); 
         document.addEventListener('mouseup', moveEnd);
     };
 
     const moving = function(ev){
-        
-        let left = ev.pageX - originLeft - boxW * 0.5,
-            top = ev.pageY - originTop - boxH * 0.5;
-        left = left < minX ? minX : (left > maxX ? maxX : left);
-        top = top < minY ? minY : (top > maxY ? maxY : top);
+
+        // 盒子移动距离  鼠标当前client距离 - 盒子初始位置 - 盒子一般宽/高
+        let left = ev.pageX - boxW * 0.5,
+            top = ev.pageY -  boxH * 0.5;
+        left = left < minL ? minL : (left > maxL ? maxL : left);
+        top = top < minT ? minT : (top > maxT ? maxT : top);
         utils.CSS(box, {
             left: left,
             top: top
